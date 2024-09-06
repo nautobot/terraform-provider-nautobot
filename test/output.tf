8ebc4cd9b81099026bc2b18393c463a3af55fe2c
TobiPeterG <github.threefold020@passmail.net>
2024-09-06 14:28:51 +0200

    Add VLAN and VLANs data sources

--- test/output.tf
+++ test/output.tf
@@ -1,3 +1,69 @@
+data "nautobot_cluster" "example" {
+  depends_on = [nautobot_cluster.new]
+  name = nautobot_cluster.new.name
+}
+
+output "cluster_details" {
+  value = data.nautobot_cluster.example
+}
+
+output "cluster_id" {
+  value = data.nautobot_cluster.example.id
+}
+
+data "nautobot_clusters" "example" {
+  depends_on = [nautobot_cluster.new]
+}
+
+output "clusters_details" {
+  value = data.nautobot_clusters.example.clusters[0]
+}
+
+output "clusters_id" {
+  value = data.nautobot_clusters.example.clusters[0].id
+}
+
+
+data "nautobot_cluster_type" "example" {
+  depends_on = [nautobot_cluster_type.new]
+  name = nautobot_cluster_type.new.name
+}
+
+output "cluster_type_details" {
+  value = data.nautobot_cluster_type.example
+}
+
+output "cluster_type_id" {
+  value = data.nautobot_cluster_type.example.id
+}
+
+data "nautobot_cluster_types" "example" {
+  depends_on = [nautobot_cluster_type.new]
+}
+
+output "cluster_types_details" {
+  value = data.nautobot_cluster_types.example.cluster_types[0]
+}
+
+output "cluster_types_id" {
+  value = data.nautobot_cluster_types.example.cluster_types[0].id
+}
+
+
+data "nautobot_manufacturer" "example" {
+  depends_on = [nautobot_manufacturer.new]
+  name = nautobot_manufacturer.new.name
+}
+
+output "manufacturer_details" {
+  value = data.nautobot_manufacturer.example
+}
+
+output "manufacturer_id" {
+  value = data.nautobot_manufacturer.example.id
+}
+
+
 data "nautobot_manufacturers" "all" {
   depends_on = [nautobot_manufacturer.new]
 }
@@ -15,6 +81,8 @@ output "data_source_example" {
     if manufacturer.name == var.manufacturer_name
   }
 }
+
+
 data "nautobot_graphql" "nodes" {
   query = <<EOF
 query {
@@ -37,29 +105,20 @@ output "data_source_graphql_vm" {
   value = jsondecode(data.nautobot_graphql.nodes.data).virtual_machines
 }
 
-data "nautobot_cluster_types" "example" {
-  depends_on = [nautobot_cluster_type.new]
+
+data "nautobot_virtual_machine" "example" {
+  depends_on = [nautobot_virtual_machine.new]
+  name = nautobot_virtual_machine.new.name
 }
 
-output "cluster_types_details" {
-  value = data.nautobot_cluster_types.example.cluster_types[0]
+output "vm_details" {
+  value = data.nautobot_virtual_machine.example
 }
 
-output "cluster_types_id" {
-  value = data.nautobot_cluster_types.example.cluster_types[0].id
+output "vm_id" {
+  value = data.nautobot_virtual_machine.example.id
 }
 
-data "nautobot_clusters" "example" {
-  depends_on = [nautobot_cluster.new]
-}
-
-output "clusters_details" {
-  value = data.nautobot_clusters.example.clusters[0]
-}
-
-output "clusters_id" {
-  value = data.nautobot_clusters.example.clusters[0].id
-}
 
 data "nautobot_virtual_machines" "example" {
   depends_on = [nautobot_virtual_machine.new]
@@ -73,54 +132,25 @@ output "vms_id" {
   value = data.nautobot_virtual_machines.example.virtual_machines[0].id
 }
 
-data "nautobot_manufacturer" "example" {
-  depends_on = [nautobot_manufacturer.new]
-  name = nautobot_manufacturer.new.name
+data "nautobot_vlan" "example" {
+  name = "pek02-106-mgmt"
 }
 
-output "manufacturer_details" {
-  value = data.nautobot_manufacturer.example
+output "vlan_details" {
+  value = data.nautobot_vlan.example
 }
 
-output "manufacturer_id" {
-  value = data.nautobot_manufacturer.example.id
+output "vlan_id" {
+  value = data.nautobot_vlan.example.id
 }
 
-data "nautobot_cluster_type" "example" {
-  depends_on = [nautobot_cluster_type.new]
-  name = nautobot_cluster_type.new.name
+data "nautobot_vlans" "example" {
 }
 
-output "cluster_type_details" {
-  value = data.nautobot_cluster_type.example
+output "vlans_details" {
+  value = data.nautobot_vlans.example.vlans[0]
 }
 
-output "cluster_type_id" {
-  value = data.nautobot_cluster_type.example.id
-}
-
-data "nautobot_cluster" "example" {
-  depends_on = [nautobot_cluster.new]
-  name = nautobot_cluster.new.name
-}
-
-output "cluster_details" {
-  value = data.nautobot_cluster.example
-}
-
-output "cluster_id" {
-  value = data.nautobot_cluster.example.id
-}
-
-data "nautobot_virtual_machine" "example" {
-  depends_on = [nautobot_virtual_machine.new]
-  name = nautobot_virtual_machine.new.name
-}
-
-output "vm_details" {
-  value = data.nautobot_virtual_machine.example
-}
-
-output "vm_id" {
-  value = data.nautobot_virtual_machine.example.id
+output "vlans_id" {
+  value = data.nautobot_vlans.example.vlans[0].id
 }
